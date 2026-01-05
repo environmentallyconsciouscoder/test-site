7f529ced6693cc8b40d5a6a7c404b20005b477d4
ecocoder <environmentallyconsciouscoder@gmail.com>
2026-01-05 00:36:00 +0000

    refactor: removed company number

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -71,7 +71,7 @@ const Footer = () => {
             </Logo>
           </Link>
             <p className="text-sm text-white">
-              © RetroSet Inc. {new Date().getFullYear()} | Company No. 16175556
+              © RetroSet Inc. {new Date().getFullYear()}
             </p>
         </div>
       </FadeIn>
